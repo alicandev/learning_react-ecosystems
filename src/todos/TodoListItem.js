4d13d6232e4a80f59c51c0a334018d2ccca17745
demirtas-dev <62238518+demirtas-dev@users.noreply.github.com>
2020-05-18 11:29:40 +0100

    `TodoList` now has a `NewTodoForm` element rendered within it's main div, and is being rendered in `App.js`.
    Styling added.

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -6,7 +6,7 @@ const TodoListItem = ({ todo }) => (
     <div className="todo-item-container">
         <h3>{ todo.text }</h3>
         <div className="buttons-container">
-            <button className="completed-button">Mark as Completed</buVtton>
+            <button className="completed-button">Mark as Completed</button>
             <button className="remove-button">Remove</button>
         </div>
     </div>
